70761b7250112b99b159c8127cd3597a7bc8fabb
AssemMahrous <assem.mahrous.cs@gmail.com>
2020-06-09 00:12:13 +0200

    Done: -some code refactoring

--- app/src/main/java/com/trial/listsectionssample/core/utils/Errors.kt
+++
@@ -1,5 +0,0 @@
-package com.trial.listsectionssample.core.utils
-
-data class Error(val error: String? = null, val errorRes: Int? = null)
-
-class ApplicationException(var error: Error) : RuntimeException()
